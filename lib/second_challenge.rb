ab372a67f40b02620e44e89e636edcb85642bc92
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 05:57:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,14 +4,14 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero =>{},
-      :hero_friends =[]
+      :hero_friends =>[]
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :hero =>{},
-      :hero_friends=>[]
+      :hero_friends =>[]
       }
   }
 
